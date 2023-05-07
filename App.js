aa37131face31e9bf5adbd9f84db9b9ca15aa6a9
QDLu <quangdunglu159@gmail.com>
2023-05-07 20:39:35 +0700

    - Process UI

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StatusBar } from 'react-native';
+import { StatusBar, View } from 'react-native';
 import HomeScreen from './src/screens/HomeScreen';
 
 export default function App() {
--- src/components/TabHome.js
+++ src/components/TabHome.js
@@ -11,76 +11,77 @@ export default function TabHome() {
   const statusBarHeight = insets.top;
 
   return (
-    <ScrollView>
-      <View style={{
-        backgroundColor: COLORS.background,
-      }}>
+    <ScrollView
+      showsVerticalScrollIndicator={false}>
+      <View
+        style={{
+          height: 428,
+          backgroundColor: COLORS.grey01,
+        }}>
         <View
           style={{
-            height: 428,
-            backgroundColor: COLORS.grey01,
+            marginHorizontal: DISTANCE.contentPadding,
+            marginBottom: DISTANCE.distance40,
           }}>
           <View
             style={{
-              marginHorizontal: DISTANCE.contentPadding,
-              marginBottom: DISTANCE.distance40,
+              width: DISTANCE.distance40,
+              height: DISTANCE.distance40,
+              backgroundColor: COLORS.white,
+              marginTop: statusBarHeight + DISTANCE.contentPadding,
+              borderRadius: 20,
+              justifyContent: 'center',
+              alignItems: 'center',
+              marginBottom: DISTANCE.distance16,
             }}>
-            <View
-              style={{
-                width: DISTANCE.distance40,
-                height: DISTANCE.distance40,
-                backgroundColor: COLORS.white,
-                marginTop: statusBarHeight + DISTANCE.contentPadding,
-                borderRadius: 20,
-                justifyContent: 'center',
-                alignItems: 'center',
-                marginBottom: DISTANCE.distance16,
-              }}>
-              <Image source={ImageAssets.back} style={{
-                width: 6,
-                height: 12,
-              }}></Image>
-            </View>
-            <Text
-              style={{
-                color: COLORS.white,
-                fontSize: 32,
-                fontFamily: FONT_FAMILY.BOLD,
-                marginBottom: DISTANCE.distance8,
-              }}>Silver Tier
-            </Text>
-            <Text
-              style={{
-                color: COLORS.grey05,
-                fontSize: 16,
-                fontFamily: FONT_FAMILY.REGULAR,
-                lineHeight: 24,
-              }}>In Silver Tier, every $1 in rental fee paid, you get 2 coins to redeem exclusive rewards.
-            </Text>
+            <Image source={ImageAssets.back} style={{
+              width: 6,
+              height: 12,
+            }}></Image>
           </View>
-          <AvailableCoinBalance />
+          <Text
+            style={{
+              color: COLORS.white,
+              fontSize: 32,
+              fontFamily: FONT_FAMILY.BOLD,
+              marginBottom: DISTANCE.distance8,
+            }}>Silver Tier
+          </Text>
+          <Text
+            style={{
+              color: COLORS.grey05,
+              fontSize: 16,
+              fontFamily: FONT_FAMILY.REGULAR,
+              lineHeight: 24,
+            }}>In Silver Tier, every $1 in rental fee paid, you get 2 coins to redeem exclusive rewards.
+          </Text>
+        </View>
+      </View>
+      <View
+        style={{
+          marginTop: -(428 - 252)
+        }}>
+        <AvailableCoinBalance />
+        <View>
+          <Text
+            style={{
+              marginHorizontal: DISTANCE.contentPadding,
+              marginVertical: DISTANCE.contentPadding,
+              lineHeight: 24,
+              fontSize: 18,
+              fontFamily: FONT_FAMILY.BOLD,
+              color: COLORS.grey01,
+            }}>Petrol</Text>
           <View>
-            <Text
-              style={{
-                marginHorizontal: DISTANCE.contentPadding,
-                marginVertical: DISTANCE.contentPadding,
-                lineHeight: 24,
-                fontSize: 18,
-                fontFamily: FONT_FAMILY.BOLD,
-                color: COLORS.grey01,
-              }}>Petrol</Text>
-            <View>
-              <FlatList
-                horizontal
-                data={dummy}
-                renderItem={({ item, index }) => {
-                  console.log('item :>> ', item);
-                  return (
-                    <Text key={item.id}>{item.title}</Text>
-                  )
-                }}
-              />
-            </View>
+            <FlatList
+              horizontal
+              data={dummy}
+              renderItem={({ item, index }) => {
+                return (
+                  <Text key={item.id}>{item.title}</Text>
+                )
+              }}
+            />
           </View>
         </View>
       </View>
